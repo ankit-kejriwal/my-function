bf789eb4171f5c130592eef405a14f8831a29d5e
ankit <ankitkejriwaal@gmail.com>
2020-10-16 22:57:00 -0400

    chnaged the port to 3001

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/say', async (req,res)=>{
     if(req.query && req.query.keyword){
